cda47ffaf815d63b18751f39c9da3858e25325e8
Steve Clay <steve@mrclay.org>
2014-02-23 23:22:43 -0500

    fix(coeditor): makes sure basepath is set early enough
    If CKEDITOR_BASEPATH is not set early enough, the editor will try to
    load its theme files via the cache handler, which will fail.

--- mod/ckeditor/views/default/js/elgg/ckeditor.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor.js
@@ -1,6 +1,3 @@
-// This global variable must be set before the editor script loading.
-CKEDITOR_BASEPATH = elgg.config.wwwroot + 'mod/ckeditor/vendors/ckeditor/';
-
 define(function(require) {
 	var elgg = require('elgg');
 	var $ = require('jquery'); require('jquery.ckeditor');
