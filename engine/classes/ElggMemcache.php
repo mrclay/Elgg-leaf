dfb23cc19394a3985dea362ed00796f8f9b3e4b0
Steve Clay <steve@mrclay.org>
2015-11-05 10:41:21 -0500

    Hack to make ElggMemcache use APC/APCu

--- engine/classes/ElggMemcache.php
+++ engine/classes/ElggMemcache.php
@@ -6,33 +6,11 @@
  * @subpackage Memcache
  */
 class ElggMemcache extends \ElggSharedMemoryCache {
-	/**
-	 * Global Elgg configuration
-	 * 
-	 * @var \stdClass
-	 */
-	private $CONFIG;
-
-	/**
-	 * Minimum version of memcached needed to run
-	 *
-	 */
-	private static $MINSERVERVERSION = '1.1.12';
-
-	/**
-	 * Memcache object
-	 */
-	private $memcache;
 
 	/**
 	 * Expiry of saved items (default timeout after a day to prevent anything getting too stale)
 	 */
 	private $expires = 86400;
-
-	/**
-	 * The version of memcache running
-	 */
-	private $version = 0;
 
 	/**
 	 * Connect to memcache.
@@ -44,68 +22,17 @@ class ElggMemcache extends \ElggSharedMemoryCache {
 	 */
 	public function __construct($namespace = 'default') {
 		global $CONFIG;
-		$this->CONFIG = $CONFIG;
 
 		$this->setNamespace($namespace);
 
 		// Do we have memcache?
-		if (!class_exists('Memcache')) {
-			throw new \ConfigurationException('PHP memcache module not installed, you must install php5-memcache');
-		}
-
-		// Create memcache object
-		$this->memcache	= new Memcache;
-
-		// Now add servers
-		if (!$this->CONFIG->memcache_servers) {
-			throw new \ConfigurationException('No memcache servers defined, please populate the $this->CONFIG->memcache_servers variable');
-		}
-
-		if (is_callable(array($this->memcache, 'addServer'))) {
-			foreach ($this->CONFIG->memcache_servers as $server) {
-				if (is_array($server)) {
-					$this->memcache->addServer(
-						$server[0],
-						isset($server[1]) ? $server[1] : 11211,
-						isset($server[2]) ? $server[2] : false,
-						isset($server[3]) ? $server[3] : 1,
-						isset($server[4]) ? $server[4] : 1,
-						isset($server[5]) ? $server[5] : 15,
-						isset($server[6]) ? $server[6] : true
-					);
-
-				} else {
-					$this->memcache->addServer($server, 11211);
-				}
-			}
-		} else {
-			// don't use _elgg_services()->translator->translate() here because most of the config hasn't been loaded yet
-			// and it caches the language, which is hard coded in $this->CONFIG->language as en.
-			// overriding it with real values later has no effect because it's already cached.
-			_elgg_services()->logger->error("This version of the PHP memcache API doesn't support multiple servers.");
-
-			$server = $this->CONFIG->memcache_servers[0];
-			if (is_array($server)) {
-				$this->memcache->connect($server[0], $server[1]);
-			} else {
-				$this->memcache->addServer($server, 11211);
-			}
-		}
-
-		// Get version
-		$this->version = $this->memcache->getVersion();
-		if (version_compare($this->version, \ElggMemcache::$MINSERVERVERSION, '<')) {
-			$msg = vsprintf('Memcache needs at least version %s to run, you are running %s',
-				array(\ElggMemcache::$MINSERVERVERSION,
-				$this->version
-			));
-
-			throw new \ConfigurationException($msg);
+		if (!function_exists('apc_store')) {
+			throw new \ConfigurationException('No apc_store');
 		}
 
 		// Set some defaults
-		if (isset($this->CONFIG->memcache_expires)) {
-			$this->expires = $this->CONFIG->memcache_expires;
+		if (isset($CONFIG->memcache_expires)) {
+			$this->expires = $CONFIG->memcache_expires;
 		}
 	}
 
@@ -154,7 +81,7 @@ class ElggMemcache extends \ElggSharedMemoryCache {
 			$expires = $this->expires;
 		}
 
-		$result = $this->memcache->set($key, $data, null, $expires);
+		$result = apc_store($key, $data, $expires);
 		if ($result === false) {
 			_elgg_services()->logger->error("MEMCACHE: SAVE FAIL $key");
 		} else {
@@ -176,8 +103,8 @@ class ElggMemcache extends \ElggSharedMemoryCache {
 	public function load($key, $offset = 0, $limit = null) {
 		$key = $this->makeMemcacheKey($key);
 
-		$result = $this->memcache->get($key);
-		if ($result === false) {
+		$result = apc_fetch($key, $success);
+		if (!$success) {
 			_elgg_services()->logger->info("MEMCACHE: LOAD MISS $key");
 		} else {
 			_elgg_services()->logger->info("MEMCACHE: LOAD HIT $key");
@@ -196,7 +123,7 @@ class ElggMemcache extends \ElggSharedMemoryCache {
 	public function delete($key) {
 		$key = $this->makeMemcacheKey($key);
 
-		return $this->memcache->delete($key, 0);
+		apc_delete($key);
 	}
 
 	/**
