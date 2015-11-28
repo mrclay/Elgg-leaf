c87e11de77135abac9ae1a8d58ec6ac56af90e33
Steve Clay <steve@mrclay.org>
2015-11-28 01:35:18 -0500

    fix(river): opening comment form auto-focuses input
    Also the button is blurred when the form is hidden.

--- js/lib/ui.river.js
+++ js/lib/ui.river.js
@@ -9,6 +9,16 @@ elgg.ui.river.init = function() {
 		url += '?' + $(this).val();
 		elgg.forward(url);
 	});
+
+	$(document).on('elgg.ui.toggle', function (e, data) {
+		if (data.$target.is('.elgg-river-responses > .elgg-form-comment-save')) {
+			if (data.$toggler.hasClass('elgg-state-active')) {
+				data.$target.find('.elgg-input-text').focus();
+			} else {
+				data.$toggler.blur();
+			}
+		}
+	});
 };
 
 elgg.register_hook_handler('init', 'system', elgg.ui.river.init);
